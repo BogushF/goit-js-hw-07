fce2fa276a1787f35a737da462a9bc82f74eebc1
BogushF <154054453+BogushF@users.noreply.github.com>
2024-04-25 11:53:41 +0200

    task4/6

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const login = event.target.elements.login.value.trim();
+  const password = event.target.elements.password.value.trim();
+  
+  if (login === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
