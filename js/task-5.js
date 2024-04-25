56f7a6267c6b55b45db4e206a1b28fc68b0d1bd9
BogushF <154054453+BogushF@users.noreply.github.com>
2024-04-25 14:16:16 +0200

    task5/6

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+const randomBackgroundColor = () => {
+  
+  const randomHexColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomHexColor;  
+  span.textContent = randomHexColor;
+}
+
+btn.addEventListener("click", randomBackgroundColor);
