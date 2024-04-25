2c4a1e12325967c221170bf143d68b498cab80e2
BogushF <154054453+BogushF@users.noreply.github.com>
2024-04-25 10:38:50 +0200

    task2/6

---
+++ js/task-1.js
@@ -0,0 +1,9 @@
+const listCategories = document.querySelectorAll(".item");
+console.log('Number of categories:', listCategories.length);
+
+listCategories.forEach((element)=>{
+  const titleCategory = element.querySelector("h2").textContent;
+console.log('Category:',titleCategory);
+  const lengthCategory = element.querySelectorAll("li").length;
+  console.log('Elements:',lengthCategory);
+});
